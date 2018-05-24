1e1a810ef77b1915ddc1c9db85ce93a3e6b1f018
tygkking <2696874339@qq.com>
2018-05-24 12:27:57 +0800

    Add refresh
    Add refresh function in peoplePage

--- pages/peoplePage/peoplePage.js
+++ pages/peoplePage/peoplePage.js
@@ -100,4 +100,33 @@ Page({
             messageVal:""
         })
     },
+
+    //下拉刷新
+    onPullDownRefresh: function () {
+        wx.showNavigationBarLoading() //在标题栏中显示加载
+        // wx.request({
+        //     url: 'https://URL',
+        //     data: {},
+        //     method: 'GET',
+        //     // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        //     // header: {}, // 设置请求的 header
+        //     success: function (res) {
+        //         // success
+        //     },
+        //     fail: function () {
+        //         // fail
+        //     },
+        //     complete: function () {
+        //         // complete
+        //         wx.hideNavigationBarLoading() //完成停止加载
+        //         wx.stopPullDownRefresh() //停止下拉刷新
+        //     },
+        // })
+        //模拟加载
+        setTimeout(function () {
+            // complete
+            wx.hideNavigationBarLoading() //完成停止加载
+            wx.stopPullDownRefresh() //停止下拉刷新
+        }, 1500);
+    },
 })
